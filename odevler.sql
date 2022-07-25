20e02152bf16a28777d51c3e4707961ba9743314
mucahitozkaya <109138108+mucahitozkaya@users.noreply.github.com>
2022-07-25 16:54:36 +0300

    Update odevler.sql

--- odevler.sql
+++ odevler.sql
@@ -1,3 +1,5 @@
+ÖDEV-1
+
 1-) Select title, description from film;
 
 2-) Select * from film where length>60 and length<75;
@@ -7,3 +9,12 @@
 4-) Select last_name from customer where first_name = 'Mary';
 
 5-) Select * from film where not (length > 50) and not (rental_rate = '2.99' or rental_rate = '4.99');
+
+
+ÖDEV-2
+
+1-) Select * from film where replacement_cost between 12.99 and 16.99;
+
+2-) Select * from actor where first_name in ('Penelope','Nick','Ed');
+
+3-) Select * from film where (rental_rate in ('0.99','2.99','4.99')) and (replacement_cost in ('12.99','15.99','28.99'));
