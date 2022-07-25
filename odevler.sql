56b83c58397513722fc24c02a754134d4918556a
mucahitozkaya <109138108+mucahitozkaya@users.noreply.github.com>
2022-07-25 16:44:59 +0300

    Create odevler.sql

---
+++ odevler.sql
@@ -0,0 +1,9 @@
+1-) Select title, description from film;
+
+2-) Select * from film where length>60 and length<75;
+
+3-) Select * from film where (rental_rate = 0.99) and (replacement_cost = 12.99 or replacement_cost = 28.99);
+
+4-) Select last_name from customer where first_name = 'Mary';
+
+5-) Select * from film where not (length > 50) and not (rental_rate = '2.99' or rental_rate = '4.99');
