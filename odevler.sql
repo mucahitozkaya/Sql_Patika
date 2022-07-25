9244b5be60a3647f21e586f2118db204a962a088
mucahitozkaya <109138108+mucahitozkaya@users.noreply.github.com>
2022-07-25 17:20:33 +0300

    Update odevler.sql

--- odevler.sql
+++ odevler.sql
@@ -18,3 +18,15 @@
 2-) Select * from actor where first_name in ('Penelope','Nick','Ed');
 
 3-) Select * from film where (rental_rate in ('0.99','2.99','4.99')) and (replacement_cost in ('12.99','15.99','28.99'));
+
+
+ÖDEV-3
+
+1-) Select * from country where country like 'A%a';
+
+2-) Select * from country where country like '_____%n';
+
+3-) Select * from film where title ilike '%t%t%t%t%';
+
+4-) Select * from film where (title like 'C%') and (length > 90) and (rental_rate = 2.99);
+
