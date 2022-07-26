d58990ba8980997591977c26688ad93e3fb31b7e
mucahitozkaya <109138108+mucahitozkaya@users.noreply.github.com>
2022-07-26 13:00:20 +0300

    Update odevler.sql

--- odevler.sql
+++ odevler.sql
@@ -44,6 +44,44 @@
 5-) Select Count(*) from city where city ilike '%R';
 
 
+ÖDEV-5
+
+1-) 
+
+Select * from film
+where title like '%n'
+order by length desc
+limit 5;
+    
+2-)
+
+Select * from film
+where title like '%n'
+order by length
+offset 5
+limit 5;
+
+3-)
+
+Select * from customer
+where store_id = 1
+order by last_name desc
+limit 4;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
