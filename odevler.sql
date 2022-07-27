96aff868df17f9a883e71773dbff06ab7bda53f7
mucahitozkaya <109138108+mucahitozkaya@users.noreply.github.com>
2022-07-27 12:57:42 +0300

    Update odevler.sql

--- odevler.sql
+++ odevler.sql
@@ -216,7 +216,19 @@ Select rental.rental_id, customer.first_name, customer.last_name from customer
 INNER JOIN rental ON customer.customer_id = rental.customer_id ;
 
 
+ÖDEV 10-)
 
+1-)
+Select city.city, country.country From city
+LEFT JOIN country ON country.country_id = city.country_id;
+
+2-)
+Select payment.payment_id, customer.first_name, customer.last_name from customer
+RIGHT JOIN payment ON customer.customer_id = payment.customer_id ;
+
+3-)
+Select rental.rental_id, customer.first_name, customer.last_name from customer
+FULL JOIN rental ON customer.customer_id = rental.customer_id ;
 
 
 
