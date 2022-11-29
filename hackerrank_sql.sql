6b308ff2a1c73a705db444e8f34f1fed22957518
mucahitozkaya <109138108+mucahitozkaya@users.noreply.github.com>
2022-11-29 23:00:28 +0300

    Update hackerrank_sql.sql

--- hackerrank_sql.sql
+++ hackerrank_sql.sql
@@ -16,3 +16,13 @@ Select salary * months as earnings, count(*) from Employee
 group by earnings 
 order by earnings desc limit 1;
 
+Select Case
+when (((A=B and A<>C) or (A=C and A<>B) or (B=C and B<>A)) and ((A+B)>C and (A+C)>B and (B+C)>A)) then 'Isosceles'
+when A = B AND B = C then 'Equilateral'
+when (A<>B and A<>C and B<>C and ((A+B)>C and (A+C)>B and (B+C)>A)) then 'Scalene'
+when (A+B)<=C or (A+C)<B or (B+C)<=A then 'Not A Triangle' end
+from TRIANGLES;
+
+Select Concat(Name, '(', UCASE(LEFT(Occupation, 1)), ')') from OCCUPATIONS order by Name;
+
+Select Concat('There are a total of ', Count(Occupation), ' ', LCASE(Occupation),'s.') From OCCUPATIONS group by Occupation order by Count(Occupation);
